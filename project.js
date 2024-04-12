7dca9235cdda1e61fd1ce0e3ec531a31c6017fec
bitandbrick <150059886+bitandbrick@users.noreply.github.com>
2024-04-12 15:34:05 +0400

    Add files via upload

---
+++ project.js
@@ -0,0 +1,28 @@
+// welcome to blot!
+
+// check out this guide to learn how to program in blot
+// https://blot.hackclub.com/editor?guide=start
+
+const width = 125;
+const height = 125;
+
+setDocDimensions(width, height);
+
+const finalLines = [];
+
+const t = new bt.Turtle();
+
+for (let i = 0; i < 52; i++) {
+  t.forward(i);
+  t.right(91);
+}
+
+// add turtle to final lines
+bt.join(finalLines, t.lines());
+
+// center piece
+const cc = bt.bounds(finalLines).cc;
+bt.translate(finalLines, [width / 2, height / 2], cc);
+
+// draw it
+drawLines(finalLines);
